61c5ccb21d6bc5cdc426b072d714f5ccadc8de5f
Benjamin Bommer <ben@crm-now.de>
2018-06-12 15:17:06 +0200

    Set default encoding of SOAP class to UTF-8

--- libraries/nusoap/nusoap.php
+++ libraries/nusoap/nusoap.php
@@ -132,8 +132,8 @@ class nusoap_base {
 	* @var      string
 	* @access   public
 	*/
-    var $soap_defencoding = 'ISO-8859-1';
-	//var $soap_defencoding = 'UTF-8';
+    // var $soap_defencoding = 'ISO-8859-1';
+	var $soap_defencoding = 'UTF-8';
 
 	/**
 	* namespaces in an array of prefix => uri
