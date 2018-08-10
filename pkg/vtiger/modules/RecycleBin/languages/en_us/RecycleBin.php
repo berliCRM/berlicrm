bc86425bc310cde99d497b1cd63237edc2b84d71
Benjamin Bommer <ben@crm-now.de>
2018-08-10 15:03:58 +0200

    added missing labels, fixed translation

--- pkg/vtiger/modules/RecycleBin/languages/en_us/RecycleBin.php
+++ pkg/vtiger/modules/RecycleBin/languages/en_us/RecycleBin.php
@@ -6,27 +6,28 @@
  * The Initial Developer of the Original Code is vtiger.
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
- * Reviewed by crm-now GmbH
  ************************************************************************************/
-$languageStrings = array(
-	'LBL_EMPTY_RECYCLEBIN' => 'Papierkorb leeren',
-	'LBL_NO_PERMITTED_MODULES' => 'Keine zulässigen Module verfügbar',
-	'LBL_NO_RECORDS_FOUND' => 'Keine Datensätze zum Wiederherstellen gefunden in',
-	'LBL_RECORDS_LIST' => 'Papierkorbinhalt',
-	'LBL_RESTORE' => 'Wiederherstellen',
-	'LBL_SELECT_MODULE' => 'Modul wählen',
-	'Recycle Bin' => 'Papierkorb',
+
+$languageStrings  = Array(
+	'LBL_EMPTY_RECYCLEBIN' => 'Empty Recycle Bin',
+	'LBL_NO_PERMITTED_MODULES' => 'No permitted modules available',
+	'LBL_NO_RECORDS_FOUND' => 'No records found to Restore in module',
+	'LBL_RECORDS_LIST' => 'Recycle Bin List',
+	'LBL_RESTORE' => 'Restore',
+	'LBL_SELECT_MODULE' => 'Select Module',
+	'Recycle Bin' => 'Recycle Bin',
 	//crm-now: missing in en_us file
-	'RecycleBin' => 'Papierkorb',
+	'RecycleBin' => 'Recycle Bin',
 );
 
 $jsLanguageStrings = array(
-	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => 'Sind Sie sicher, dass Sie die Datensätze wiederherstellen wollen?',
-	'JS_LBL_RESTORE_RECORD_CONFIRMATION' => 'Sind Sie sicher, dass Sie diesen Datensatz wiederherstellen wollen?',
-	'JS_MSG_EMPTY_RB_CONFIRMATION' => 'Sind Sie sicher, dass Sie die Datensätze endgültig löschen wollen?',
-	'JS_RESTORING_RECORDS' => 'Datensätze werden wieder hergestellt',
-	'of' => 'von',
-	'to' => 'bis',
+	'JS_MSG_EMPTY_RB_CONFIRMATION' => 'Are you sure you want to permanently remove all the deleted records from your database?',
+	'JS_LBL_RESTORE_RECORDS_CONFIRMATION' => 'Are you sure you want to restore the records?',
+	'JS_LBL_RESTORE_RECORD_CONFIRMATION' => 'Are you sure you want to restore this record?',
+	'JS_RESTORING_RECORDS' => 'Restoring records',
+	'of' => 'of',
+	'to' => 'to',
 );
+
 
 ?>
