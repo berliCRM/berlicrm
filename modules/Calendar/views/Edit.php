45043b96f5ff6cec081eecc358385835cec0492f
Benjamin Bommer <ben@crm-now.de>
2018-09-07 13:11:26 +0200

    Fix for event's end-time changing in editview.

--- modules/Calendar/views/Edit.php
+++ modules/Calendar/views/Edit.php
@@ -93,6 +93,7 @@ Class Calendar_Edit_View extends Vtiger_Edit_View {
 			$viewer->assign('VIEW_MODE', $viewMode);
 		}
 
+        $userChangedEndDateTime = (!empty($record)) ? 1 : $request->get('userChangedEndDateTime');
 		//If followup value is passed from request to process the value and sent to client
 		$requestFollowUpDate = $request->get('followup_date_start');
 		$requestFollowUpTime = $request->get('followup_time_start');
@@ -114,6 +115,7 @@ Class Calendar_Edit_View extends Vtiger_Edit_View {
 			$viewer->assign('SHOW_FOLLOW_UP',FALSE);
 		}
 
+        $viewer->assign('USER_CHANGED_END_DATE_TIME',$userChangedEndDateTime);
 		$viewer->assign('FOLLOW_UP_DATE',$followUpDate);
 		$viewer->assign('FOLLOW_UP_TIME',$followUpTime);
 		$viewer->assign('RECURRING_INFORMATION', $recordModel->getRecurrenceInformation());
