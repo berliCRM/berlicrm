c7ceac89bde010eb308b6dc1c2cf2f5660681654
Benjamin Bommer <ben@crm-now.de>
2018-11-07 13:33:08 +0100

    added Import Cron correction, new Webservices and vtiger_smsnotifier_servers table alteration to db_update script.

--- db_update.php
+++ db_update.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html><html><?php
+require_once("includes/main/WebUI.php");
 require_once('include/utils/utils.php');
 include_once('vtlib/Vtiger/Module.php');
 include_once('include/Webservices/Utils.php');
@@ -13,6 +14,81 @@ $adb->pquery($query, array());
 $query = "UPDATE vtiger_field SET uitype = '2' WHERE columnname = 'lastname'";
 $adb->pquery($query, array());
 echo " done";
+
+
+//  Import Cron correction
+echo "Proper text for Import Cron (if it exists)... ";
+$query = "UPDATE vtiger_cron_task SET description = ? WHERE module = ?;";
+$res = $adb->pquery($query, array('The recommended frequency for Imports is 15 minutes.', 'Import'));
+if ($res) {
+	echo "done<br>";
+} else {
+	echo "failed<br>";
+}
+
+// add new Webservices
+echo "add new web services ";
+// check existance (for migrated clients)
+$query = "SELECT * FROM `vtiger_ws_operation` WHERE `name` = 'retrievedocattachment'";
+$res = $adb->pquery($query, array());
+if ($adb->num_rows($res) > 0) {
+	// add web service: retrievedocattachment
+	$operationId = vtws_addWebserviceOperation('retrievedocattachment','include/Webservices/RetrieveDocAttachment.php','berli_retrievedocattachment','Get','0');
+	vtws_addWebserviceOperationParam($operationId,'id','string','1');
+	vtws_addWebserviceOperationParam($operationId,'returnfile','string','2');
+	echo "retrievedocattachment added";
+}
+else {
+	echo "retrievedocattachment already exists";
+}
+// check existance (for migrated clients)
+$query = "SELECT * FROM `vtiger_ws_operation` WHERE `name` = 'update_product_relations'";
+$res = $adb->pquery($query, array());
+if ($adb->num_rows($res) > 0) {
+	// add web service: update_product_relations
+	$operationId = vtws_addWebserviceOperation('update_product_relations','include/Webservices/Custom/ProductRelation.php','vtws_update_product_relations','POST','0');
+	vtws_addWebserviceOperationParam($operationId,'productid','string','1');
+	vtws_addWebserviceOperationParam($operationId,'relids','string','2');
+	vtws_addWebserviceOperationParam($operationId,'preserve','string','3');
+	echo "update_product_relations added";
+}
+else {
+	echo "update_product_relations already exists";
+}
+// check existance (for migrated clients)
+$query = "SELECT * FROM `vtiger_ws_operation` WHERE `name` = 'get_multi_relations'";
+$res = $adb->pquery($query, array());
+if ($adb->num_rows($res) > 0) {
+	// add web service: get_multi_relations
+	$operationId = vtws_addWebserviceOperation('get_multi_relations','include/Webservices/Custom/getMultiRelations.php','berli_get_multi_relations','Get','0');
+	vtws_addWebserviceOperationParam($operationId,'id','string','1');
+	echo "get_multi_relations added";
+}
+else {
+	echo "get_multi_relations already exists";
+}
+
+// update vtiger_smsnotifier_servers structure (if required)
+echo "<br>Update vtiger_smsnotifier_servers structure (if required)<br>Table ";
+$query = "SHOW COLUMNS FROM vtiger_smsnotifier_servers LIKE 'countryprefix';";
+$res = $adb->pquery($query, array());
+if ($res) {
+	echo "found";
+	if ($adb->num_rows($res) < 1) {
+		echo " and update required";
+		$query = "ALTER TABLE vtiger_smsnotifier_servers ADD `countryprefix` VARCHAR (5) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '';"
+		$res = $adb->pquery($query, array());
+		if ($res) {
+			echo "<br>Update successful";
+		} else {
+			echo "<br>Update NOT successful";
+		}
+	} else {
+		echo " and update NOT required";
+	}
+} else {
+	echo "not found";
+}
 
 echo "<br>update Tag version to 10.. ";
 $query = "UPDATE `vtiger_version` SET `tag_version` = 'berlicrm-1.0.0.10'";
