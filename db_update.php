7efd637cf7048bf83a33ac3436282885cac4266c
AlexKay85 <ak@crm-now.de>
2019-05-17 10:36:54 +0200

    added Mailchimp package update to script

--- db_update.php
+++ db_update.php
@@ -318,9 +318,41 @@ foreach($moduleFolders as $moduleFolder) {
 }
 echo 'module install Verteiler done <br>';
 
+echo 'module update Mailchimp start<br>';
+//update Mailchimp module
+$moduleFolders = array('packages/vtiger/mandatory', 'packages/vtiger/optional');
+foreach($moduleFolders as $moduleFolder) {
+	if ($handle = opendir($moduleFolder)) {
+		while (false !== ($file = readdir($handle))) {
+			$packageNameParts = explode(".",$file);
+			if($packageNameParts[count($packageNameParts)-1] != 'zip'){
+				continue;
+			}
+			array_pop($packageNameParts);
+			$packageName = implode("",$packageNameParts);
+			if ($packageName =='Mailchimp') {
+				$packagepath = "$moduleFolder/$file";
+				$package = new Vtiger_Package();
+				$module = $package->getModuleNameFromZip($packagepath);
+				if($module != null) {
+					$moduleInstance = Vtiger_Module::getInstance($module);
+					if($moduleInstance) {
+						updateVtlibModule($module, $packagepath);
+					} 
+					else {
+						installVtlibModule($module, $packagepath);
+					}
+				}
+			}
+		}
+		closedir($handle);
+	}
+}
+echo 'module update Mailchimp done <br>';
 
-echo "<br>update Tag version to 17.. ";
-$query = "UPDATE `vtiger_version` SET `tag_version` = 'berlicrm-1.0.0.17'";
  
+echo "<br>update Tag version to 18. ";
+$query = "UPDATE `vtiger_version` SET `tag_version` = 'berlicrm-1.0.0.18'";
+
 $adb->pquery($query, array());
 echo " Tag version done.<br>";
