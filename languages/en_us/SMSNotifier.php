b34c39fa9ce0ca89abc38486d5d6196f5b23b5a4
Benjamin Bommer <ben@crm-now.de>
2019-05-31 10:19:45 +0200

    fixed the issue that a filter set by search parameters was not considered in list view added a SMS count display

--- languages/en_us/SMSNotifier.php
+++ languages/en_us/SMSNotifier.php
@@ -8,16 +8,38 @@
  * All Rights Reserved.
  *************************************************************************************/
 $languageStrings = array(
-	'LBL_ADD_MORE_FIELDS' => 'Add more fields',
-	'LBL_CHECK_STATUS' => 'Check Status',
-	'LBL_SELECT_THE_PHONE_NUMBER_FIELDS_TO_SEND' => 'Select the phone number fields to send',
-	'LBL_SEND_SMS_TO_SELECTED_NUMBERS' => 'Send SMS to the selected numbers',
-	'LBL_SERVER_CONFIG' => 'Server Configuration',
-	'LBL_SMSNOTIFIER_INFORMATION' => 'SMS Information',
+	// Basic Strings
+	'LBL_SEND' => 'Send',
+	'LBL_SMS_TO_SELECTED_NUMBERS' => 'SMS to the selected numbers',
 	'LBL_STEP_1' => 'Step 1',
 	'LBL_STEP_2' => 'Step 2',
+	'LBL_SELECT_THE_PHONE_NUMBER_FIELDS_TO_SEND' => 'Select the phone number fields to send',
 	'LBL_TYPE_THE_MESSAGE' => 'Type the message',
 	'LBL_WRITE_YOUR_MESSAGE_HERE' => 'write your message here',
+	'LBL_ADD_MORE_FIELDS' => 'Add more fields',
+	'LBL_SERVER_CONFIG' => 'Server Configuration',
+	'LBL_PROVIDER_MESSAGE' => 'Provider Message:',
+	'LBL_PROVIDER_ERROR_MESSAGE' => 'Provider Error Message:',
+	'LBL_MESSAGE_SENT' => 'Message has been sent',
+	'LBL_SERVER_CONFIG' => 'Server Configuration', 
+
+	//DetailView Actions
+	'LBL_CHECK_STATUS' => 'Check Status',
 	'message' => 'Message',
+
+	//Blocks
+	'LBL_SMSNOTIFIER_INFORMATION' => 'SMS Information',
 	'SINGLE_SMSNotifier' => 'SMS Notifier',
+	
+	//status messages
+	'LBL_PROVIDER_MESSAGE' => 'Provider message',
+	'LBL_PROVIDER_ERROR_MESSAGE' => 'Error message from provider',
+	'LBL_OTHER_PROVIDER_MESSAGE' => 'Provider information',
+	'LBL_MESSAGE_SENT' => 'SMS has been send',
 );
+
+$jsLanguageStrings = array(
+	'of' => 'of',
+	'to' => 'to',
+);
+?>
