bd2c24306e6f4a975f3947bf3fd23bf2e54ea061
Benjamin Bommer <ben@crm-now.de>
2019-10-15 14:54:35 +0200

    Unified legend placement in widgets.

--- layouts/vlayout/modules/Vtiger/resources/dashboards/Widget.js
+++ layouts/vlayout/modules/Vtiger/resources/dashboards/Widget.js
@@ -366,7 +366,7 @@ Vtiger_Widget_Js('Vtiger_Funnel_Widget_Js',{},{
 			legend: {
 				show: true,
 				location: 'ne',
-				placement: 'outsideGrid',
+				placement: 'outside',
 				labels:labels
 			}
 		});
@@ -438,7 +438,8 @@ Vtiger_Widget_Js('Vtiger_Pie_Widget_Js',{},{
             },
 			legend: {
 				show: true,
-				location: 'e'
+				location: 'ne',
+				placement: 'outside'
 			},
 			title : chartData['title']
 		});
