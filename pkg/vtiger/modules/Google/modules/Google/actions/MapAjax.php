7ad88b6aafe318b55120d77a1382cc56abd1ecb4
Alex Kay <ak@crm-now.de>
2022-12-23 11:13:50 +0100

    fixed an JS error when Maps widget couldn't find an address

--- pkg/vtiger/modules/Google/modules/Google/actions/MapAjax.php
+++ pkg/vtiger/modules/Google/modules/Google/actions/MapAjax.php
@@ -27,7 +27,7 @@ class Google_MapAjax_Action extends Vtiger_BasicAjax_Action {
     function getLocation(Vtiger_Request $request) {
         $address = Google_Map_Helper::getLocation($request);
 		$mapapikey= Settings_Google_Module_Model::getGoogleApikey();
-        return empty($address) ? array("mapapikey"=> $mapapikey) : array("address" => join(",", $address),"mapapikey"=> $mapapikey);
+        return empty($address) ? array("mapapikey"=> $mapapikey, 'address' => '') : array("address" => join(",", $address),"mapapikey"=> $mapapikey);
     }
     
     public function validateRequest(Vtiger_Request $request) { 
