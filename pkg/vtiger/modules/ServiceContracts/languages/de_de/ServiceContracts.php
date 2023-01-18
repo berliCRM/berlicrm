5b972a9d8d6e1eb85df2eea4cfd6734c98700e1d
Eugen Dipner <ed@crm-now.de>
2023-01-18 16:21:42 +0100

    corrected the languages on all needed files for "ServiceContacts" module and add files that were missing.

---
+++ pkg/vtiger/modules/ServiceContracts/languages/de_de/ServiceContracts.php
@@ -0,0 +1,55 @@
+<?php
+/* +***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ * Reviewed by crm-now GmbH
+ * *********************************************************************************** */
+$languageStrings = array(	
+	'Actual Duration' => 'ggw. Dauer (in Tagen)',
+	'Contract No' => 'Vertragsnummer',
+	'End Date' => 'Enddatum',
+	'LBL_ADD_RECORD' => 'Servicevertrag hinzufügen',
+	'LBL_RECORDS_LIST' => 'Servicevertragsliste',
+	'LBL_SERVICE_CONTRACT_INFORMATION' => 'Servicevertragsinformation',
+	'Planned Duration' => 'geplante Dauer (in Tagen)',
+	'Progress'=> 'Fortschritt (in %)',
+	'Service Contracts' => 'Serviceverträge',
+	'ServiceContracts' => 'Serviceverträge',
+	'SINGLE_ServiceContracts' => 'Servicevertrag',
+	'Start Date' => 'Startdatum',
+	'Total Units' => 'Gesamtzahl',
+	'Tracking Unit' => 'Einheit',
+	'Used Units' => 'Verbrauch',
+	
+         //Patch provided by http://trac.vtiger.com/cgi-bin/trac.cgi/ticket/7884
+        'Due date' => 'Fälligkeitsdatum', 
+        //Picklist values 
+        'Hours' => 'Stunden', 
+        'Days' => 'Tage', 
+        'Incidents' => 'Ereignisse', 
+
+        'Support' => 'Support', 
+        'Services' => 'Services', 
+        'Administrative' => 'administrativ', 
+
+        'Undefined' => 'undefiniert', 
+        'In Planning' => 'in Planung', 
+        'In Progress' => 'in Bearbeitung', 
+        'On Hold' => 'in Wartestellung', 
+        'Complete' => 'abgeschlossen', 
+        'Archived' => 'archiviert', 
+	
+	//crm-now: added labels for translation not in engl. files
+	'Related to' => 'bezogen auf',
+	'Normal' => 'normal',
+);
+
+$jsLanguageStrings = array(
+	'of' => 'von',
+	'to' => 'bis',
+);
+?>
