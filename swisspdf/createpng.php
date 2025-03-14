70b7c399842585b7ace60ad37e0722e7b953e051
Eugen <dipner.eugen@web.de>
2025-03-14 15:37:51 +0100

    fix syntax errors

--- swisspdf/createpng.php
+++ swisspdf/createpng.php
@@ -23,17 +23,19 @@ $qrBill = QrBill\QrBill::create();
 // Add creditor information
 // Who will receive the payment and to which bank account?
 $qrBill->setCreditor(
-        QrBill\DataGroup\Element\CombinedAddress::create(
-            $pdfDataObj['organisation']['name'],
-            $pdfDataObj['organisation']['hnr+street'],
-            $pdfDataObj['organisation']['zip+state'],
-            $pdfDataObj['organisation']['country'],
-        ));
+    QrBill\DataGroup\Element\CombinedAddress::create(
+        $pdfDataObj['organisation']['name'],
+        $pdfDataObj['organisation']['hnr+street'],
+        $pdfDataObj['organisation']['zip+state'],
+        $pdfDataObj['organisation']['country']
+    )
+);
 
 $qrBill->setCreditorInformation(
-        QrBill\DataGroup\Element\CreditorInformation::create(
-            $pdfDataObj['qriban'], // This is a special QR-IBAN. Classic IBANs will not be valid here.
-        ));
+    QrBill\DataGroup\Element\CreditorInformation::create(
+        $pdfDataObj['qriban'] // Ensure this QR-IBAN is in the correct format
+    )
+);
 
 // Add debtor information
 // Who has to pay the invoice? This part is optional.
@@ -48,7 +50,7 @@ $qrBill->setUltimateDebtor(
             $pdfDataObj['contact']['zip'],
             $pdfDataObj['contact']['state'],
             $pdfDataObj['contact']['country'],
-            $pdfDataObj['contact']['org'],
+            $pdfDataObj['contact']['org']
         ));
 
 // Add payment amount information
@@ -56,7 +58,7 @@ $qrBill->setUltimateDebtor(
 $qrBill->setPaymentAmountInformation(
         QrBill\DataGroup\Element\PaymentAmountInformation::create(
             $pdfDataObj['payment']['currency'],
-            floatval(str_replace(",", ".",$pdfDataObj['payment']['amount'])),
+            floatval(str_replace(",", ".",$pdfDataObj['payment']['amount']))
         ));
 
 // Add payment reference
@@ -64,7 +66,7 @@ $qrBill->setPaymentAmountInformation(
 $referenceNumber = QrBill\Reference\QrPaymentReferenceGenerator::generate(
             NULL,  // You receive this number from your bank (BESR-ID). Unless your bank is PostFinance, in that case use NULL.
             //'313947143000901' // A number to match the payment with your internal data, e.g. an invoice number
-            $pdfDataObj['bill_number'],
+            $pdfDataObj['bill_number']
 );
 
 $qrBill->setPaymentReference(
@@ -81,7 +83,7 @@ $qrBill->setAdditionalInformation(
         )
 );
 
-    // Now get the QR code image and save it as a file.
+// Now get the QR code image and save it as a file.
     try{
         $filepath = 'storage/temp';
         if (!file_exists($filepath)) {
@@ -91,11 +93,10 @@ $qrBill->setAdditionalInformation(
         }
     $qrBill->getQrCode()->writeFile('storage/temp/qr'.$pdfDataObj['bill_number'].'.png');
     } catch (Exception $e) {
+
         $datei = fopen("test/testData.txt","a+");
     fwrite($datei, print_r($qrBill->getViolations(), TRUE));
     fclose($datei);
-}
+    }
 return 'storage/temp/qr'.$pdfDataObj['bill_number'].'.png';
-
-
 }
