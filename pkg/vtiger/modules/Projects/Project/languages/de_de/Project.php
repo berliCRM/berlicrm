e92539282c0fd6bad77745796243815060a0a29f
Eugen <dipner.eugen@web.de>
2026-08-21 14:38:00 +0200

    fixed language of Projects module

--- pkg/vtiger/modules/Projects/Project/languages/de_de/Project.php
+++ pkg/vtiger/modules/Projects/Project/languages/de_de/Project.php
@@ -9,78 +9,78 @@
  * Reviewed by crm-now GmbH
  *************************************************************************************/
 $languageStrings = array(
-	// Basic Strings
-	'SINGLE_Project' => 'Projekt',
-	'LBL_ADD_RECORD' => 'Projekt hinzufügen',
-	'LBL_ADD_TASK_RECORD' => 'Projektaufgabe hinzufügen',
-	'LBL_RECORDS_LIST' => 'Projektliste',
+    // Basic Strings
+    'SINGLE_Project' => 'Projekt',
+    'LBL_ADD_RECORD' => 'Projekt hinzufügen',
+    'LBL_RECORDS_LIST' => 'Projektliste',
 
-	// Blocks
-	'LBL_PROJECT_INFORMATION' => 'Projektdetails',
+    // Blocks
+    'LBL_PROJECT_INFORMATION' => 'Projektdetails',
 
-	//Field Labels
-	'Project Name' => 'Projektname',
-	'Start Date' => 'Startdatum',
-	'Target End Date' => 'Zielenddatum',
-	'Actual End Date' => 'aktuelles Enddatum',
-	'Project No' => 'Projektnummer',
-	'Target Budget' => 'Zielbudget',
-	'Project Url' => 'Projekt URL',
-	'Progress' => 'Fortschritt',
-        'LBL_MILESTONES_LIST'          => 'Meilensteineliste'           , 
-        'LBL_TASKS_HIGH'               => 'hohe Priorität'              , 
-        'LBL_TASKS_NORMAL'             => 'normal Priorität'            , 
-        'LBL_TASKS_LOW'                => 'geringe Priorität'           , 
-        'LBL_TASKS_OTHER'              => 'andere Priorität'            , 
-        'LBL_SELECT_PROGRESS'          => 'Fortschritt auswählen'       , 
+    //Field Labels
+    'Project Name' => 'Projektname',
+    'Start Date' => 'Startdatum',
+    'Target End Date' => 'Zielenddatum',
+    'Actual End Date' => 'aktuelles Enddatum',
+    'Project No' => 'Projektnummer',
+    'Target Budget' => 'Zielbudget',
+    'Project Url' => 'Projekt URL',
+    'Progress' => 'Fortschritt',
+    'LBL_MILESTONES_LIST'          => 'Meilensteineliste'           ,
+    'LBL_TASKS_HIGH'               => 'hohe Priorität'              ,
+    'LBL_TASKS_NORMAL'             => 'normal Priorität'            ,
+    'LBL_TASKS_LOW'                => 'geringe Priorität'           ,
+    'LBL_TASKS_OTHER'              => 'andere Priorität'            ,
+    'LBL_SELECT_PROGRESS'          => 'Fortschritt auswählen'       ,
 
-	//Summary Information
-	'LBL_TASKS_OPEN' => 'offene Aufgaben',
-	'LBL_TASKS_DUE' => 'fällige Aufgaben',
-	'LBL_TASKS_COMPLETED' => 'erledigte Aufgaben',
-	'LBL_PEOPLE' => 'Menschen',
+    //Summary Information
+    'LBL_TASKS_OPEN' => 'offene Aufgaben',
+    'LBL_TASKS_DUE' => 'fällige Aufgaben',
+    'LBL_TASKS_COMPLETED' => 'erledigte Aufgaben',
+    'LBL_PEOPLE' => 'Menschen',
 
-	//Related List
-	'LBL_CHARTS' => 'Charts',
-	'LBL_TASKS_LIST' => 'Aufgabenliste',
-	'LBL_MILESTONES' => 'Meilensteine',
-	'LBL_TASKS' => 'Aufgaben',
-	'LBL_STATUS_IS' => 'Status ist',
-	'LBL_STATUS' => 'Status',
-	'LBL_TICKET_PRIORITY' => 'Priorität',
-	'LBL_MORE' => 'mehr',
-	
-	//Summary View Widgets
-	'LBL_DOWNLOAD_FILE' => 'Datei runterladen',
-	'Project Task Name' => 'Projektaufgabe',
-	'Project Task Status' => 'Status',
+    //Related List
+    'LBL_CHARTS' => 'Charts',
+    'LBL_TASKS_LIST' => 'Aufgabenliste',
+    'LBL_MILESTONES' => 'Meilensteine',
+    'LBL_TASKS' => 'Aufgaben',
+    'LBL_STATUS_IS' => 'Status ist',
+    'LBL_STATUS' => 'Status',
+    'LBL_TICKET_PRIORITY' => 'Priorität',
+    'LBL_MORE' => 'mehr',
+
+    //Summary View Widgets
+    'LBL_DOWNLOAD_FILE' => 'Datei runterladen',
+    'Project Task Name' => 'Projektaufgabe',
+    'Project Task Status' => 'Status',
     'LBL_NO_RELATED' => 'Keine bezogenen',
     'Project Milestone Name' => 'Projektmeilenstein',
     'Milestone Date' => 'Meilensteindatum',
-    
-	//crm-now: missing entries in en_us file
-	'Project' => 'Projekte',
-	'administrative' => 'administrativ',
-	'operative' => 'operativ',
-	'other' => 'andere',
-	'low' => 'gering',
-	'normal' => 'normal',
-	'high' => 'hoch',
-	'description' => 'Beschreibung',
-	'prospecting' => 'in Vorbereitung',
-	'initiated' => 'begonnen',
-	'in progress' => 'in Bearbeitung',
-	'waiting for feedback' => 'warte auf Zuarbeit',
-	'on hold' => 'gestoppt',
-	'completed' => 'erledigt',
-	'delivered' => 'geliefert',
-	'archived' => 'archiviert',
+    'Add Project Task' => 'Projektaufgabe hinzufügen',
+
+    //crm-now: missing entries in en_us file
+    'Project' => 'Projekte',
+    'administrative' => 'administrativ',
+    'operative' => 'operativ',
+    'other' => 'andere',
+    'low' => 'gering',
+    'normal' => 'normal',
+    'high' => 'hoch',
+    'description' => 'Beschreibung',
+    'prospecting' => 'in Vorbereitung',
+    'initiated' => 'begonnen',
+    'in progress' => 'in Bearbeitung',
+    'waiting for feedback' => 'warte auf Zuarbeit',
+    'on hold' => 'gestoppt',
+    'completed' => 'erledigt',
+    'delivered' => 'geliefert',
+    'archived' => 'archiviert',
     'Related to' => 'bezogen auf',
 
 );
 
 $jsLanguageStrings = array(
-	'of' => 'von',
-	'to' => 'bis',
+    'of' => 'von',
+    'to' => 'bis',
 );
 ?>
